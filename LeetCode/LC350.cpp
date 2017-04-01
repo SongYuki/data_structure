cc99d88a0514c58e2abd8ee82c37d88b1a50be97
sunyx2016 <sunyx2016@gmail.com>
2017-04-01 23:56:30 +0800

    leetcode 0

--- LeetCode/LC350.cpp
+++ LeetCode/LC350.cpp
@@ -1,19 +1,84 @@
-#include<unordered_set>
-class Solution
-{
+/**
+m: nums1.size n: nums2.size
+
+Hash table solution:
+Time: O(m + n) Space: O(m + n)
+**/
+
+class Solution {
 public:
-	vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-    if(nums1.size() > nums2.size()) return intersect(nums2, nums1);
-    vector<int> ret;
-    unordered_map<int,int> map1;
-    for(int num:nums1) map1[num]++;
-    for(int num:nums2) {
-        if(map1.find(num)!=map1.end() && map1[num]>0) {
-            ret.push_back(num);
-            map1[num]--;
-        }
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> dict;
+        vector<int> res;
+        for(int i = 0; i < (int)nums1.size(); i++) dict[nums1[i]]++;
+        for(int i = 0; i < (int)nums2.size(); i++)
+            if(--dict[nums2[i]] >= 0) res.push_back(nums2[i]);
+        return res;
     }
-    return ret;
-}
 };
 
+
+/**
+Hash table solution2:
+Time: O(m + n) Space: O(m)
+**/
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> dict;
+        vector<int> res;
+        for(int i = 0; i < (int)nums1.size(); i++) dict[nums1[i]]++;
+        for(int i = 0; i < (int)nums2.size(); i++)
+            if(dict.find(nums2[i]) != dict.end() && --dict[nums2[i]] >= 0) res.push_back(nums2[i]);
+        return res;
+    }
+};
+
+/**
+Sort and two pointers Solution:
+Time: O(max(m, n) log(max(m, n))) Space: O(m + n)
+**/
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int n1 = (int)nums1.size(), n2 = (int)nums2.size();
+        int i1 = 0, i2 = 0;
+        vector<int> res;
+        while(i1 < n1 && i2 < n2){
+            if(nums1[i1] == nums2[i2]) {
+                res.push_back(nums1[i1]);
+                i1++;
+                i2++;
+            }
+            else if(nums1[i1] > nums2[i2]){
+                i2++;
+            }
+            else{
+                i1++;
+            }
+        }
+        return res;
+    }
+};
+
+/**
+C++ solution using stl
+**/
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        if(!nums1.size() || !nums2.size())
+            return ans;
+        ans.resize(nums1.size() + nums2.size());
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int>::iterator it = set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), ans.begin());
+        ans.resize(it-ans.begin());
+        return ans;
+    }
+};
